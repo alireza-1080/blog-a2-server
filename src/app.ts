9179147399501502efa1ad186fa99b734b6360ed
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-25 06:55:10 -0700

    3/25/2025 6:55 AM

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import apiRouter from './routes/api.js'
 import session from './middlewares/session.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -14,6 +15,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(express.static('public'))
 
 app.use(session)
