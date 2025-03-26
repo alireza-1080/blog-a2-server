1dbfa75d1470cedef860fd5515b7927d682dd0d5
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-26 06:32:32 -0700

    3/26/2025 6:32 AM

--- src/middlewares/lastSessionUpdate.ts
+++ src/middlewares/lastSessionUpdate.ts
@@ -1,3 +1,9 @@
+declare module 'express-session' {
+  interface SessionData {
+    lastVisit?: number;
+  }
+}
+
 import { Request, Response, NextFunction } from 'express-serve-static-core'
 
 const lastSessionUpdate = (req: Request, _res: Response, next: NextFunction) => {
