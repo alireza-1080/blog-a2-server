dc04c8e05b7700785bf51e8df2346b347c5bc461
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-26 06:14:51 -0700

    test cookie 3/26/2025 6:14 AM

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,4 +7,14 @@ router.get('/', async (req: Request, res: Response) => {
   res.json(`Session has been updated`)
 })
 
+router.get('/test', async (req: Request, res: Response) => {
+  res.cookie('chocolate_cookie', 'chocolate_almond_etc', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: 14 * 24 * 60 * 60 * 1000
+  })
+  res.json(`Session has been updated`)
+})
+
 export default router
