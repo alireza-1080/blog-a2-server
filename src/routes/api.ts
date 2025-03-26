8d3d30f7964cc9f5d2db1cab2caaa2e8fe058b16
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-26 05:56:34 -0700

    3/26/2025 5:56 AM

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,15 +5,7 @@ import { prisma } from '../services/prisma.service.js'
 const router = express.Router()
 
 router.get('/', async (req: Request, res: Response) => {
-  const testData = await prisma.blogPost.findMany()
-  console.log(testData)
-  res.cookie('test', 'testValue', {
-    httpOnly: true,
-    sameSite: 'none',
-    path:"/",
-    secure: true
-  })
-  res.json(testData)
+  res.json(`Session has been updated`)
 })
 
 export default router
