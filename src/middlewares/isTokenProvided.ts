8441bf87de0c1de110b68a0a5486b4b964c2d5cc
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-31 01:10:49 -0700

    3/31/2025 1:10 AM

--- src/middlewares/isTokenProvided.ts
+++ src/middlewares/isTokenProvided.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express-serve-static-core'
 
 const isTokenProvided = (req: Request, res: Response, next: NextFunction) => {
-  const auth_token = req.cookies?.auth_token
+  const auth_token = req.headers.authorization?.split(' ')[1] || req.cookies?.auth_token || ""
 
   if (!auth_token) {
     req.isTokenProvided = false
