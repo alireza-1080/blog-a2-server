8e9e1c5611a3165ef6ca12592580cf449d052e59
Alireza Aberoumand <maya7192@yahoo.com>
2025-04-04 21:09:29 -0700

    4/4/2025 9:09 PM

--- src/utils/corsDependencies.ts
+++ src/utils/corsDependencies.ts
@@ -1,12 +1,12 @@
 import { CorsOptions } from 'cors'
 
-const allowedOrigins = ['https://blog-a2.vercel.app', 'http://localhost:3000', 'https://blog-a2-server.up.railway.app']
+const allowedOrigins = ['https://blog-a2.vercel.app', 'https://blog-a2-server.up.railway.app']
 
 const corsOptions: CorsOptions = {
   origin: (origin: string | undefined, cb: (error: Error | null, allow?: boolean) => void) => {
-    if (!origin) {
-      return cb(null, true)
-    }
+    // if (!origin) {
+    //   return cb(null, true)
+    // }
 
     if (allowedOrigins.indexOf(origin as string) !== -1) {
       cb(null, true)
